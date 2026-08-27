96818e8ae89e24581d1192a5ad519d16c2341b4c
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-27 15:43:55 +1200

    Format vite config

--- vite.config.js
+++ vite.config.js
@@ -61,7 +61,10 @@ export default defineConfig(({ command, mode }) => {
             "papaparse": "papaparse"
           },
           // keep css output name stable for the "./dist/style.css" export/import paths
-          assetFileNames: (assetInfo) => (assetInfo.name?.endsWith(".css") ? "style.css" : "assets/[name][extname]"),
+          assetFileNames: (assetInfo) =>
+            assetInfo.name?.endsWith(".css")
+              ? "style.css"
+              : "assets/[name][extname]",
         },
       },
     },
